ae35cff973d1833430a6bb45149b32a6a669c48d
Xander Kaylan <xxk3990@rit.edu>
2023-06-06 15:49:26 -0400

    LOGIN SYSTEM WORKS

--- server/router.js
+++ server/router.js
@@ -3,9 +3,10 @@ const worlds = require('./controllers/worlds-controller.js')
 
 const router = (app) => {
     app.get('/users', users.getUsers)
-    app.post('/addUser', users.addUser)
+    app.post('/addUser', users.createAccount)
     app.post('/addWorld', worlds.addWorld)
     app.get('/worlds', worlds.getWorlds)
+    app.post('/login', users.login)
 }
 
 module.exports = router;
