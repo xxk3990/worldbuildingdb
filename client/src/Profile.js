0dabdb123bf2c53ef8e837cff474dbc95186bcd1
Xander Kaylan <xxk3990@rit.edu>
2023-07-13 16:39:15 -0400

    profile page timer now decreases with intervals in useEffect

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -5,6 +5,7 @@ import "./styles/profile.css"
 import { useNavigate } from 'react-router-dom';
 import { handleGet } from './services/requests-service';
 import { checkAuth } from './services/auth-service';
+import { handleLogout } from './services/auth-service';
 export default function Profile() {
     localStorage.setItem("page", "profile")
     const [userProfile, setUserProfile] = useState([]);
@@ -12,9 +13,26 @@ export default function Profile() {
     const expiration = localStorage.getItem("expiration")
     const loginTime = localStorage.getItem("loginTime")
 
+    const minutesRemaining = (now) => {
+        const timeSinceLogin = now - loginTime; //get milliseconds since user logged in
+        const minutesSince = timeSinceLogin / 60000; //convert it to minutes
+        const minsLeft = expiration - minutesSince; //subtract # of minutes logged in from token exp time
+        return Math.round(minsLeft); //round off decimals
+    }
+    const [minutes, setMinutes] = useState(minutesRemaining(Date.now()))
     useEffect(() => {
         document.title = "Profile – Worldbuilding DB"
-    })
+        const interval = setInterval(() => {
+            const decrease = minutes - 1;
+            setMinutes(decrease)
+        }, 60000) //every minute, reduce # of minutes left by 1
+        if(minutes === 0) {
+            console.log("clear interval condition reached");
+            clearInterval(interval)
+            handleLogout()
+            navigate("/login");
+        }
+    }, [minutes])
 
     const currentUserID = localStorage.getItem("user")
  
@@ -31,12 +49,6 @@ export default function Profile() {
     useEffect(() => {
         getProfile()
     }, [])
-    const minutesRemaining = (now, expiration, loginTime) => {
-        const timeSinceLogin = now - loginTime; //get milliseconds since user logged in
-        const minutesSince = timeSinceLogin / 60000; //convert it to minutes
-        const minsLeft = expiration - minutesSince; //subtract # of minutes logged in from token exp time
-        return Math.round(minsLeft); //round off decimals
-    }
     return (
         <div className='Profile'>
             <section className='profile-container'>
@@ -44,9 +56,10 @@ export default function Profile() {
                     
                 <h4>Name: {userProfile.first_name} {userProfile.last_name}</h4>
                 <h4>Username: {userProfile.username}</h4>
-                <h4>Email: {userProfile.email}</h4>
-                <h4>Automatic logout occurs {expiration} minutes after login. You will be logged out in&nbsp; 
-                    {minutesRemaining(Date.now(), expiration, loginTime)} minutes (reload to refresh).</h4>
+                <h4 className='user-email'>Email: {userProfile.email}</h4>
+                <span className = "logout-info">
+                    <h4>{minutes === 1 ? `Automatic logout occurs ${expiration} minutes after login. You will be logged out in ${minutes} minute` : `Automatic logout occurs ${expiration} minutes after login. You will be logged out in ${minutes} minutes.`}</h4>
+                </span>
             </section>
         </div>
     )
